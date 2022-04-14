b6d263d494bbab63595a44df0aed965adc5b7007
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-04-14 17:56:37 +0530

    feat: build from solita

---
+++ instructions/index.ts
@@ -0,0 +1,4 @@
+export * from './initializeAdmin';
+export * from './initializeEvent';
+export * from './initializeHost';
+export * from './mintTicket';
--- src/conditions.ts
+++
@@ -1,4 +0,0 @@
-export const conditions = {
-    title: "This is the Title",
-    uri: "This is the URI",
-};
